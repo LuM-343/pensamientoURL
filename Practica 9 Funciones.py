90b7517488760e3931d986ea533827d4d2bee490
LuM-343 <manuelgonz3101@gmail.com>
2025-04-23 10:43:51 -0600

    Create Practica 9 Funciones.py
    Primer ejercicio

---
+++ Practica 9 Funciones.py
@@ -0,0 +1,13 @@
+#Luis Manuel Velásquez González 1502325
+#Ejercicio 1
+print("--------------------")
+print("Ejercicio 1")
+
+def par_impar (n):
+    if n%2==0:
+        print("Es un número par")
+    else:
+        print ("Es un número impar")
+        
+num=int(input("Ingrese un numero: "))
+par_impar(num)
