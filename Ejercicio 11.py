2bc6fe98e08818205c6e94997ac02ada7326c110
LuM-343 <manuelgonz3101@gmail.com>
2025-05-07 11:39:19 -0600

    Update Ejercicio 11.py
    Ejercicio 2

--- Ejercicio 11.py
+++ Ejercicio 11.py
@@ -26,3 +26,91 @@ try:
     print(f"El tiempo es de {ejercicio1.calcularTiempo()}")
 except ValueError:
     print("Datos no válidos")
+
+
+#Ejercicio 2
+#Calculadora
+
+import math
+
+class OperacionCientifica ():
+    def __init__(self, numUno, numDos):
+        self.numUno=numUno
+        self.numDos=numDos
+        
+    def Calcular(self):
+        try:
+            print(f"Suma= {self.numUno+self.numDos}")
+            print(f"Resta= {self.numUno-self.numDos}")
+            print(f"Multiplicación= {self.numUno*self.numDos}")
+            print(f"División= {self.numUno/self.numDos}")
+        except TypeError:
+            print("Datos no válidos")
+        except ZeroDivisionError:
+            print("No se puede dividir por 0")
+            
+class RaizCuadrada (OperacionCientifica):
+    def __init__(self, numUno, numDos):
+        super().__init__(numUno, numDos)
+    
+    def Operar(self):
+        try:
+            raiz=math.sqrt(self.numUno)
+            print(raiz)
+        except TypeError:
+            print("Datos no válidos")
+        except ValueError:
+            print("El numero no puede ser negativo")
+
+class Potencia (OperacionCientifica):
+    def __init__(self, numUno, numDos):
+        super().__init__(numUno, numDos)
+    
+    def Operar(self):
+        try:
+            potencia=self.numUno**self.numDos
+            print(potencia)
+        except TypeError:
+            print("Datos no válidos")
+            
+class Logaritmo (OperacionCientifica):
+    def __init__(self, numUno, numDos):
+        super().__init__(numUno, numDos)
+    
+    def Operar(self):
+        try:
+            logaritmo= math.log(self.numUno,self.numDos)
+            print(logaritmo)
+        except TypeError:
+            print("Datos no válidos")
+        except ValueError:
+            print("El numero no puede ser negativo")
+            
+class Logaritmo (OperacionCientifica):
+    def __init__(self, numUno, numDos):
+        super().__init__(numUno, numDos)
+    
+    def Operar(self):
+        try:
+            logaritmo= math.log(self.numUno,self.numDos)
+            print(logaritmo)
+        except TypeError:
+            print("Datos no válidos")
+        except ValueError:
+            print("El numero no puede ser negativo")
+            
+class Factorial (OperacionCientifica):
+    def __init__(self, numUno, numDos):
+        super().__init__(numUno, numDos)
+    
+    def Operar(self):
+        try:
+            factorial= math.factorial(self.numUno)
+            print(factorial)
+        except TypeError:
+            print("Datos no válidos")
+        except ValueError:
+            print("El numero no puede ser negativo")
+            
+hola=OperacionCientifica(6,2)
+hola.Calcular()
